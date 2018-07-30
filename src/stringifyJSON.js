823b62524e489357f65c3a328121af6e38122800
cstung21 <cstung21@gmail.com>
2018-07-30 11:51:38 -0700

    complete stringify JSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,6 +4,8 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+
+  
   // your code goes here
   if (obj === null) {
     return 'null';
@@ -15,9 +17,41 @@ var stringifyJSON = function(obj) {
   
   if (Array.isArray(obj)) {
     if (obj.length === 0) {
-      return '[]'
-    }
+      return '[]';
+    } else {
+      var arrayStr = '[';
+      
+      for (var i = 0; i < obj.length; i++) {
+        if (i === obj.length - 1) {
+          arrayStr += (stringifyJSON(obj[i]) + ']');
+        } else {
+          arrayStr += (stringifyJSON(obj[i]) + ',');
+        }
+      }
+      
+      return arrayStr;
+    }    
   }
+  
+   if (typeof obj === 'object') {
+    var objStr = '{'
+    var objKeys = Object.keys(obj);
+    
+    if (objKeys.length === 0) {
+      return '{}';
+    }
+    
+    for (var i = 0; i < objKeys.length; i++) {
+      if (typeof obj[objKeys[i]] === 'function' || obj[objKeys[i]] === undefined) {
+        objStr += '';
+      } else if (i === objKeys.length - 1) {
+        objStr += (stringifyJSON(objKeys[i]) + ':' + (stringifyJSON(obj[objKeys[i]])));
+      } else {
+        objStr += (stringifyJSON(objKeys[i]) + ':' + (stringifyJSON(obj[objKeys[i]]))) + ',';
+      }
+    }
+    return objStr + '}'
+   }
   
   
 
